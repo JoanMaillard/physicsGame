bf53e3bc1e94619bf090e5cee00d308b19fcb8c5
lgraziano <leandro.graziano@epfl.ch>
2017-12-07 11:50:14 +0100

    Bike n' wheel

--- src/main/java/ch/epfl/cs107/play/game/actor/bikeRider/Bike.java
+++ src/main/java/ch/epfl/cs107/play/game/actor/bikeRider/Bike.java
@@ -3,10 +3,14 @@ package ch.epfl.cs107.play.game.actor.bikeRider;
 import ch.epfl.cs107.play.game.actor.Actor;
 import ch.epfl.cs107.play.game.actor.ActorGame;
 import ch.epfl.cs107.play.game.actor.GameEntity;
+import ch.epfl.cs107.play.math.Circle;
 import ch.epfl.cs107.play.math.Entity;
+import ch.epfl.cs107.play.math.EntityBuilder;
+import ch.epfl.cs107.play.math.PartBuilder;
 import ch.epfl.cs107.play.math.Polygon;
 import ch.epfl.cs107.play.math.Transform;
 import ch.epfl.cs107.play.math.Vector;
+import ch.epfl.cs107.play.math.World;
 import ch.epfl.cs107.play.window.Canvas;
 
 public class Bike extends GameEntity implements Actor{
@@ -14,27 +18,43 @@ public class Bike extends GameEntity implements Actor{
 	public static final float MAX_WHEEL_SPEED = 20f;
 	static boolean right = true;
 	private Entity entity;
-	static Vector positionInit; 
+	static Vector positionInit;
+	private World world;
 	
-	Polygon polygon = new Polygon(
-			0.0f, 0.5f,
-			0.5f, 1.0f,
-			0.0f, 2.0f,
-			-0.5f, 1.0f
-			) ;
+	
 	
 	public Bike(ActorGame game, boolean fixed) {
 		super(game, fixed);
-		// TODO Auto-generated constructor stub
+		positionInit = new Vector(0f, 0f);
+		build();
 	}
 	
 	public Bike(ActorGame game, boolean fixed, Vector Position) {
 		super(game, fixed);
 		positionInit = Position;
-		// TODO Auto-generated constructor stub
+		build();
 	}
 
+	private void build() {
+		EntityBuilder entityBuilder = world.createEntityBuilder() ;
+		 PartBuilder partBuilder = entity.createPartBuilder() ;
+		 entityBuilder.setFixed(false) ;		 
+		 entityBuilder.setPosition(positionInit);		 
+	     
+	     entity = entityBuilder.build() ;
+	     partBuilder = entity.createPartBuilder() ;
+	     Polygon polygon = new Polygon(
+	 			0.0f, 0.5f,
+	 			0.5f, 1.0f,
+	 			0.0f, 2.0f,
+	 			-0.5f, 1.0f
+	 			) ;
 
+	     partBuilder.setShape(polygon) ;
+	     partBuilder.setFriction(0.5f) ;
+	     partBuilder.build() ;
+	
+	}
 
 	@Override
 	public Transform getTransform() {
