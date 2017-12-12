c673fdf1512ddf21707945d1d5dc0c865f64f998
lgraziano <leandro.graziano@epfl.ch>
2017-12-12 10:00:38 +0100

    oc

--- src/main/java/ch/epfl/cs107/play/game/actor/bikeRider/Terrain.java
+++ src/main/java/ch/epfl/cs107/play/game/actor/bikeRider/Terrain.java
@@ -64,20 +64,25 @@ public class Terrain extends GameEntity{
                         break;
                     case 3: //Change the level!
                         Polyline terrainLv3 = new Polyline(
-						-1000.0f, -1000.0f,
-						-1000.0f, 0.0f,
-						0.0f, 0.0f,
-						3.0f, 1.0f,
-						8.0f, 1.0f,
-						15.0f, 3.0f,
-						16.0f, 3.0f,
-						25.0f, 0.0f,
-						35.0f, -5.0f,
-						50.0f, -5.0f,
-						55.0f, -4.0f,
-						65.0f, 0.0f,
-						6500.0f, -1000.0f
-						);
+                        		-15f, 0f,
+                 				-5, 0f,
+                 				-3.5f, -10f,
+                 				1f, -19f,
+                 				2.57f, -23.3f,
+                 				6.77f, -25.2f,
+                 				13.35f, -23.87f,
+                 				17f, -22f,
+                 				17f, -30f,
+                 				25f, -30f,
+                 				25f, -22f,
+                 				27.56f, -23.68f,
+                 				100f, -25f,
+                 				100f, -100f,
+                 				-15f, -100f
+                 				);
+                        for (int i = 0; i < 7 ; i++) {
+                        new Spike(getOwner(), true, new Vector(17.5f+i, -29.7f));
+                        }
                         Finish finish3 = new Finish(getOwner(), true, new Vector(-5f, 0.3f));
                         finish = finish3;
                         break;
