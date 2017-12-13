8f5468eb995c76f32b51601c35d565d68ed77a9d
lgraziano <leandro.graziano@epfl.ch>
2017-12-13 08:50:11 +0100

    skin

---
+++ src/main/java/ch/epfl/cs107/play/game/actor/bikeRider/SkinChooser.java
@@ -0,0 +1,38 @@
+package ch.epfl.cs107.play.game.actor.bikeRider;
+
+import java.awt.event.KeyEvent;
+
+import ch.epfl.cs107.play.game.actor.ActorGame;
+import ch.epfl.cs107.play.game.actor.ImageGraphics;
+import ch.epfl.cs107.play.math.Entity;
+import ch.epfl.cs107.play.math.Vector;
+import ch.epfl.cs107.play.window.Canvas;
+import ch.epfl.cs107.play.window.Window;
+
+public class SkinChooser {
+
+	
+	private Icon bikeIcon;
+	private Icon beanIcon;
+	static boolean skin;
+	
+	public SkinChooser(ActorGame game) {
+		bikeIcon = new Icon(game, true, new Vector(-10f, 0f));
+		beanIcon = new Icon(game, false, new Vector(10f, 0f));
+	}
+	
+	public void choose(Window window) {
+		if (window.getKeyboard().get(KeyEvent.VK_LEFT).isPressed()) {
+			if (skin != true) { 
+				skin = true;
+			}
+		}
+	if (window.getKeyboard().get(KeyEvent.VK_RIGHT).isPressed()) {
+		if (skin != false) {
+			skin = false;}
+	}	
+	}
+	
+	
+	
+}
